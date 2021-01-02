81661812b67a1570b584c7c2aa0cbbca1a7f8ce0
Your Name <sunilbpoojari@gmail.com>
2021-01-02 09:21:58 +0530

    Added header as config file

--- myntra.py
+++ myntra.py
@@ -5,6 +5,7 @@ import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 #If you are using Chrome version 87, please download ChromeDriver 87.0.4280.88
 CORE = "https://www.myntra.com/"
 PRODUCT = "https://www.myntra.com/gateway/v2/product/12681772"
@@ -41,25 +42,21 @@ for cookie in s.cookies:
 
 
 print("Calling product end point with requests")
-header = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'en-US,en;q=0.9',
-        'cache-control': 'max-age=0',
-        'dnt': '1',
-        'sec-ch-ua': '"Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'none',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
-}
+
+with open("config.json","r") as f:
+    config = json.load(f)
+header = config
 
 response  = s.get(PRODUCT,timeout=10,headers=header)
 print(response.status_code)
 with open("product.json","w") as f :
     json.dump(response.json(),f)
 from pprint import  pprint
-# pprint(response.json())
+product_data = (response.json())
+if response:
+    print(f"product name {product_data.get('style').get('name')}")
+    print(f"product price {product_data.get('style').get('mrp')}")
+    for size in product_data.get('style').get('sizes'):
+        print(f"\nsize is {size.get('label')}  ",end= "")
+        for seller in size.get('sizeSellerData'):
+            print(seller.get('discountedPrice')   ,end="",sep= "   ")
